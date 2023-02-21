e70551f87d62f072a5c5b2bacca7e5dd1db301d1
naveenkumar sivarajan <sivarajan.naveenkumar@mahindra.com>
2023-02-21 18:07:46 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,45 @@
+resource "azurerm_postgresql_server" "postgresql_server" {
+  name                              = var.name
+  location                          = var.location
+  resource_group_name               = var.resource_group_name
+  administrator_login               = var.administrator_login
+  administrator_login_password      = var.administrator_login_password
+  sku_name                          = var.sku_name
+  version                           = var.postgresql_version
+  storage_mb                        = var.storage_mb
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = true
+  auto_grow_enabled                 = true
+  public_network_access_enabled     = false
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  infrastructure_encryption_enabled = true
+}
+
+resource "azurerm_postgresql_configuration" "postgresql_configuration" {
+  name                = "connection_throttling"
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_server.postgresql_server.name
+  value               = "on"
+}
+
+resource "azurerm_postgresql_configuration" "postgresql_configuration" {
+  name                = "log_checkpoints"
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_server.postgresql_server.name
+  value               = "on"
+}
+
+resource "azurerm_postgresql_configuration" "postgresql_configuration" {
+  name                = "log_connections"
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_server.postgresql_server.name
+  value               = "on"
+}
+
+resource "azurerm_postgresql_configuration" "postgresql_configuration" {
+  name                = "log_disconnections"
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_server.postgresql_server.name
+  value               = "on"
+}
